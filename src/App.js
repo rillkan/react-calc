e5ffb74b7ef5669bb965afa0b3327f9dae6cdbaf
Reagan Kan <mjkan96@gmail.com>
2024-05-31 23:43:27 +0800

    Handle when "0" input and "." input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-//App.js
 import { useReducer } from "react"
 import DigitButton from "./DigitButton"
+
 import "./App.css"
 
 //global variable "ACTIONS" to access the key-value pairs
@@ -16,12 +16,17 @@ function reducer(state, { type, payload }) {
   console.log("Action dispatched:", type, payload); // Log the dispatched action
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if (payload.digit === '0' && state.currentOperand === '0') return state //Handle multiple 0 inputs and avoid 0000 as currentOperand
+      if (payload.digit === '.' && state.currentOperand.includes(".")) return state //Handle "." as currentOperand only has 1 "."
       const newOperand = `${state.currentOperand || ""}${payload.digit}`;
       console.log("New current operand:", newOperand); // Log the new current operand
       return {
         ...state,
         currentOperand: newOperand
       };
+
+
+
     default:
       return state;
   }
@@ -40,22 +45,18 @@ function App() {
       <button className="span-two">AC</button>
       <button>DEL</button>
       <button>÷</button>
-      <button>*</button>
-      <button>+</button>
-      <button>-</button>
-
       <DigitButton digit="1" sendDispatch={dispatch} />
       <DigitButton digit="2" sendDispatch={dispatch} />
       <DigitButton digit="3" sendDispatch={dispatch} />
-
+      <button>*</button>
       <DigitButton digit="4" sendDispatch={dispatch} />
       <DigitButton digit="5" sendDispatch={dispatch} />
       <DigitButton digit="6" sendDispatch={dispatch} />
-
+      <button>+</button>
       <DigitButton digit="7" sendDispatch={dispatch} />
       <DigitButton digit="8" sendDispatch={dispatch} />
       <DigitButton digit="9" sendDispatch={dispatch} />
-
+      <button>-</button>
       <DigitButton digit="." sendDispatch={dispatch} />
       <DigitButton digit="0" sendDispatch={dispatch} />
       <button className="span-two">=</button>
--- src/DigitButton.js
+++ src/DigitButton.js
@@ -3,7 +3,7 @@
 import { ACTIONS } from './App'
 //Rendering a button for each digit
 export default function DigitButton({ sendDispatch, digit }) {
-  const handleClick = () => {
+  const handleClick = () => { //Handling the click event
     console.log("Button clicked:", digit); // Log the clicked digit
     sendDispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } });
   };
